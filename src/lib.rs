5631f2d01a7a3700d6230b4943d4e40eb09d48c4
Gabriel Fu <hfu.gabriel@gmail.com>
2022-10-07 11:33:09 +0800

    add unit test

--- src/lib.rs
+++ src/lib.rs
@@ -16,3 +16,27 @@ fn rugraphlib(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
 }
 
 
+#[cfg(test)]
+mod test {
+    use super::*;
+    use ndarray::arr2;
+
+    #[test]
+    fn test_edmonds_karp() {
+        let capacity = arr2(&[
+            [13., 10.,  8.,  4.,  4.,  0.,  1.,  0.],
+            [ 2., 13., 10., 14.,  8.,  9., 15., 11.],
+            [10.,  8.,  8., 14.,  4., 13., 10.,  0.],
+            [ 6., 13.,  8.,  0., 12., 11., 13.,  2.],
+            [ 1., 13.,  0.,  8.,  1.,  4.,  7.,  6.],
+            [ 6.,  0.,  0.,  1.,  0., 10.,  8., 10.],
+            [ 4.,  9., 12.,  6.,  7., 15., 12., 15.],
+            [ 6., 10., 15., 10., 13., 11., 11.,  6.],
+        ]);
+        let s = 0;
+        let t = 7;
+
+        let out = algorithms::edmonds_karp(&capacity.view(), s, t);
+        assert_eq!(out, 27.);
+    }
+}
