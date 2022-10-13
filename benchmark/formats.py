cae63fcb0f5b16647524bf30445e8d10e2792e4b
gabrielfu <hfu.gabriel@gmail.com>
2022-10-13 14:30:50 +0800

    use perfplot for benchmarking

---
+++ benchmark/formats.py
@@ -0,0 +1,39 @@
+import re
+import io
+from rich.console import Console
+from rich.table import Table
+from perfplot._main import PerfplotData
+
+
+def format_time(dt: float, precision: int=3):
+    """Formats time to unit. Taken from `timeit` module."""
+    units = {"nsec": 1e-9, "usec": 1e-6, "msec": 1e-3, "sec": 1.0}
+    scales = [(scale, unit) for unit, scale in units.items()]
+    scales.sort(reverse=True)
+    for scale, unit in scales:
+        if dt >= scale:
+            break
+
+    return "%.*g %s" % (precision, dt / scale, unit)
+
+
+def format_perf_data(perf_data: PerfplotData):
+    """Formats PerfplotData with pretty time format."""
+    table = Table(show_header=True)
+    table.add_column("n")
+    for label in perf_data.labels:
+        table.add_column(label)
+
+    for n, t in zip(perf_data.n_range, perf_data.timings_s.T):
+        lst = [str(n)] + [format_time(tt) for tt in t]
+        table.add_row(*lst)
+
+    f = io.StringIO()
+    console = Console(file=f)
+    console.print(table)
+    return f.getvalue()
+
+def format_snake_case(string: str):
+    # return string.lower().replace("-", "_").
+    string = re.sub(r'(?<=[a-z])(?=[A-Z])|[^a-zA-Z]', ' ', string.replace("'", "")).strip().replace(' ', '_')
+    return ''.join(string.lower())
--- benchmark/main.py
+++ benchmark/main.py
@@ -3,7 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 os.environ["RUST_BACKTRACE"] = "full"
 
-from benchmark import benchmark
+import perfplot
+from formats import format_perf_data, format_snake_case
 import algorithms
 from algorithms.graph import Graph
 import rsgraph
@@ -12,7 +13,8 @@ from networkx.exception import NetworkXUnbounded
 
 
 def benchmark_edmonds_karp():
-    print("Benchmarking: Edmonds-Karp")
+    name = "Edmonds-Karp Algorithm"
+    print(f"Benchmarking: {name}")
     
     def setup(n):
         # source
@@ -33,116 +35,105 @@ def benchmark_edmonds_karp():
 
         return capacity, s, t
     
-    n = 32
-    capacity, s, t = setup(n)
-    print(f"Graph size: {n}")
-    
-    def nx_flow():
-        G = nx.from_numpy_array(capacity, create_using=nx.DiGraph())
-        return nx.maximum_flow_value(G, s, t, capacity="weight")
-    
-    def nx_ek():
+    def nx_func(capacity, s, t):
         G = nx.from_numpy_array(capacity, create_using=nx.DiGraph())
         return nx.algorithms.flow.edmonds_karp(G, s, t, capacity="weight").graph["flow_value"]
     
-    def python():
+    def py_func(capacity, s, t):
         return algorithms.edmonds_karp(capacity, s, t)
     
-    def rust():
+    def rsgraph_func(capacity, s, t):
         return rsgraph.edmonds_karp(capacity, s, t)
     
-    func_dict = {
-        "nx_flow": nx_flow,
-        "nx_ek": nx_ek,
-        "python": python,
-        "rust": rust,
-    }
-
-    # Benchmark
-    for name, func in func_dict.items():
-        benchmark(func, name=name)
-    print()
+    perf_data = perfplot.bench(
+        setup=setup,
+        kernels=[nx_func, py_func, rsgraph_func],
+        labels=["nx", "python", "rsgraph"],
+        n_range=[2, 3, 4],
+        xlabel="n",
+        equality_check=None,
+    )
+    print(format_perf_data(perf_data))
+    perf_data.title = name
+    perf_data.save(filename=f"./images/perf_{format_snake_case(name)}.png", logy=True)
     
 
 def benchmark_bellman_ford():
-    print("Benchmarking: Bellman-Ford")
+    name = "Bellman-Ford Algorithm"
+    print(f"Benchmarking: {name}")
     
     def setup(n):
         rng = np.random.default_rng(seed=0)
         adj = rng.integers(low=0, high=16, size=(n, n)).astype(np.float64)
         source = 0
         return adj, source
-
-    n = 32
-    adj, source = setup(n)
-    print(f"Graph size: {n}")
     
-    def nx_bf():
+    def nx_func(adj, source):
         try:
             G = nx.from_numpy_array(adj, create_using=nx.DiGraph())
             return nx.single_source_bellman_ford(G, source)
         except NetworkXUnbounded:
             return None
 
-    def python():
+    def py_func(adj, source):
         try:
             g = Graph.from_adj_matrix(adj)
             return algorithms.bellman_ford(g, source)
         except algorithms.NegativeCycleException:
             return None
 
-    def rust():
+    def rsgraph_func(adj, source):
         try:
             return rsgraph.bellman_ford(adj, source)
         except rsgraph.NegativeCycleException:
             return None
     
-    func_dict = {
-        "nx_bf": nx_bf,
-        "python": python,
-        "rust": rust,
-    }
-
-    # Benchmark
-    for name, func in func_dict.items():
-        benchmark(func, name=name)
-    print()
+    perf_data = perfplot.bench(
+        setup=setup,
+        kernels=[nx_func, py_func, rsgraph_func],
+        labels=["nx", "python", "rsgraph"],
+        n_range=[2, 3, 4],
+        xlabel="n",
+        equality_check=None,
+    )
+    print(format_perf_data(perf_data))
+    perf_data.title = name
+    perf_data.save(filename=f"./images/perf_{format_snake_case(name)}.png", logy=True)
     
 
 def benchmark_kruskal():
-    print("Benchmarking: Kruskal")
-    
+    name = "Kruskal's Algorithm"
+    print(f"Benchmarking: {name}")
+
     def setup(n):
         rng = np.random.default_rng(seed=0)
         adj = rng.integers(low=0, high=16, size=(n, n)).astype(np.float64)
+        # undirected graph
+        adj[np.tril_indices(n, -1)] = adj.T[np.tril_indices(n, -1)]
         # no loop
         adj[np.diag_indices(n)] = 0
-        # undirected graph
-        adj[np.tril_indices(n, -1)] = adj[np.triu_indices(n, 1)]
         return adj
 
-    n = 32
-    adj = setup(n)
-    print(f"Graph size: {n}")
-    
-    def nx_kruskal():
+    def nx_func(adj):
         G = nx.from_numpy_array(adj, create_using=nx.Graph())
         mst = nx.minimum_spanning_tree(G)
         mst = nx.to_numpy_array(mst)
         return mst
 
-    def rust():
-        return rsgraph.kruskal(adj)
+    def rsgraph_func(adj):
+        mst = rsgraph.kruskal(adj)
+        return mst
     
-    func_dict = {
-        "nx": nx_kruskal,
-        "rust": rust,
-    }
-
-    # Benchmark
-    for name, func in func_dict.items():
-        benchmark(func, name=name)
-    print()
+    perf_data = perfplot.bench(
+        setup=setup,
+        kernels=[nx_func, rsgraph_func],
+        labels=["nx", "rsgraph"],
+        n_range=[2, 3, 4],
+        xlabel="n",
+    )
+    print(format_perf_data(perf_data))
+    perf_data.title = name
+    perf_data.save(filename=f"./images/perf_{format_snake_case(name)}.png", logy=True)
 
 
 if __name__ == "__main__":
